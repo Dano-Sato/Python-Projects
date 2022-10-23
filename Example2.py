d3a2acf2c7598f7dc5c27463f81bb815c16ae101
Dano-Sato <ten0102@naver.com>
2022-10-23 23:05:53 +0900

    Drag_Drop

--- Example2.py
+++ Example2.py
@@ -10,12 +10,16 @@ class Brick(QGraphicsRectItem):
         self.title.setFont(BrickTitleFont)
         self.text = QGraphicsTextItem("testetsettsetstsetsetstetstststs")
         self.text.setTextWidth(130)
-        self.setBrush(Qt.black)
-        self.setPen(QPen(Qt.white,2))
+        self.setBrush(QColor(random.randint(0,75),random.randint(0,75),random.randint(0,75))) # 배경색 정함
+        self.setPen(QPen(Qt.white,1))
     def setRect(self,x,y,w,h):
         super().setRect(x,y,w,h)
         self.title.setPos(x+5,y+5)
         self.text.setPos(x+10,y+30)
+    def setTitle(str):
+        self.title.setPlainText(str)
+    def setText(str):
+        self.text.setPlainText(str)
     def addToScene(self,scn):
         scn.addItem(self)
         scn.addItem(self.text)
@@ -25,27 +29,45 @@ class Brick(QGraphicsRectItem):
         scn.removeItem(self.text)
         scn.removeItem(self.title)
 
-class Game(Genesis):
+class App(Genesis):
     def initUI(self):
+
+        self.resize(1200,800)
+        self.setWindowTitle("LayBricks")
         b = Brick()
         b.setRect(10,10,150,150)
         scene = QGraphicsScene()
         b.addToScene(scene)
+        self.Todo = []
+        self.Ongoing = []
+        self.Done = []
         self.view = QGraphicsView()
         self.view.setScene(scene)
+        self.view.viewport().installEventFilter(self)
 
-        self.resize(400,400)
-        self.setWindowTitle("LayBricks")
-        self.layout = XVLayout(QPushButton("Hello World"))
+        self.layout = XVLayout()
         self.layout.addWidget(self.view)
         self.setLayout(self.layout)
+        
+        self.draggedObject = None
 
+    def eventFilter(self, obj, event):
+        if obj is self.view.viewport():
+            if event.type() == QEvent.MouseButtonPress:
+                if event.buttons() & Qt.LeftButton:
+                    #Mouse Press Event 
+                    print('mouse press event = ', event.pos())
+            elif event.type() == QEvent.MouseButtonRelease:
+                #Mouse Release Event
+                print('mouse release event = ', event.pos())
+
+        return QWidget.eventFilter(self, obj, event)
 
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    mwindow = Game()
+    mwindow = App()
     mwindow.show()
     
     app.exec_()

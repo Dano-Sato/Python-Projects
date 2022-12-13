695c4b31ccd185cf6e93d834bb5f41236fdf8977
Dano-Sato <ten0102@naver.com>
2022-12-13 14:55:10 +0900

    View Change

--- Dance/Game1.py
+++ Dance/Game1.py
@@ -10,26 +10,43 @@ class Appli(Genesis):
         self.testSqr.setColor(QColor(50,50,50))
         self.testSqr.setEdge(QPen(Qt.white,10))
         self.scene.addItem(self.testSqr)
-        self.layout = XVLayout()
-        self.layout.addWidget(self.scene.view)
-        self.setLayout(self.layout)
         
+        self.scene2 = XGraphicsScene(0,0,1200,800)
+        self.sqr2 = XGraphicsRectItem()
+        self.sqr2.setRect(50,50,200,200)
+        self.sqr2.setColor(QColor(200,10,10))
+        self.scene2.addItem(self.sqr2)
+
+        self.layout = XVLayout()
+        self.setLayout(self.layout)
+        self.currentView = None
+        self.changeView(self.scene.view)
         self.x=200
         self.y=200
-        
+
+    def changeView(self,view):
+        if self.currentView != None:
+            self.currentView.setParent(None)
+        self.currentView = view
+        self.layout.addWidget(view)
     def update(self):
         super().update()
 
+    '''
     def drawEvent(self, event):
         a = self.drawAction
         a()
         return
     def drawAction(self):
         self.drawString(self.x,self.y,"test")
-        
+    '''
+            
     def mousePressEvent(self,event):
         if event.button() == Qt.LeftButton:
-            print('test')
+            if self.currentView ==self.scene.view:
+                self.changeView(self.scene2.view)
+            else:
+                self.changeView(self.scene.view)
     def keyPressEvent(self,event):
         if event.key()==Qt.Key_W:
             self.y-=4
